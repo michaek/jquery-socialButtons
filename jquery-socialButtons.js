8b34df0cd7c0fe1b8321f1b20359271aa3f81ea0
Michael Hellein <michael@bearded.com>
2011-05-20 08:11:14 -0400

    added like links (facebook's is pretty rudimentary)

--- jquery-socialButtons.js
+++ jquery-socialButtons.js
@@ -10,12 +10,14 @@
   var services = {
     facebook: {
       display_name: 'Facebook',
-      data_url: 'http://api.facebook.com/restserver.php?method=links.getStats&format=json-strings&urls=',
+      data_url: 'http://api.facebook.com/restserver.php?method=links.getStats&format=json-strings&urls={{url}}',
+      like_url: 'http://www.facebook.com/plugins/like.php?origin={{url}}&relation=parent.parent&transport=postmessage&font=lucida%20grande&href={{url}}&layout=standard&node_type=link&sdk=joey&show_faces=false',
       count_key: 'total_count'
     },
     twitter: { 
       display_name: 'Twitter',
-      data_url: 'http://urls.api.twitter.com/1/urls/count.json?url=',
+      data_url: 'http://urls.api.twitter.com/1/urls/count.json?url={{url}}',
+      like_url: 'http://twitter.com/share?original_referer={{url}}&url={{url}}&via=',
       count_key: 'count'
     }
   };
@@ -47,7 +49,6 @@
   $.fn.socialButtons = function(options){
     options = $.extend({
       url: window.location.href,
-      // url: 'http://www.kickstarter.com/projects/bearded/new-digital-fonts-from-rare-letterpress-wood-type',
       services: serviceKeys,
       forceRequest: false,
       friendlyCounts: true,
@@ -59,10 +60,10 @@
       
       var $buttons = $(this).addClass('social-buttons');
 
-      var $label = $('<div class="share">Share</div>')
+      var $label = $('<span class="share">Share</span>')
         .appendTo($buttons);
       
-      var $total_wrap = $('<div class="count total" />')
+      var $total_wrap = $('<span class="count total" />')
         .append('<span class="label">Total</span>')
         .append('<span class="value" />')
         .appendTo($buttons);
@@ -74,8 +75,9 @@
         if (services[service_name]) {
           var service = services[service_name];
           
-          var $count_wrap = $('<div class="count" />')
+          var $count_wrap = $('<a class="count" />')
             .addClass(service_name)
+            .attr('href', service.like_url.replace(/{{url}}/g, options.url))
             .append('<span class="label">'+service.display_name+'</span>')
             .append('<span class="value" />')
             .insertBefore($total_wrap);
@@ -85,7 +87,7 @@
           // don't make another request if we already have the stats
           if (socialStats[service_name][options.url] == undefined || options.forceRequest) {
             // get from the jsonp url
-            $.ajax(service.data_url+escape(options.url), {
+            $.ajax(service.data_url.replace(/{{url}}/g, options.url), {
               dataType: 'jsonp',
               success: function(data){
                 // facebook returns an array, but we want a single object
@@ -103,6 +105,8 @@
                   count: count,
                   raw: data
                 };
+                
+                console.log(data);
                 
                 // set count for display
                 $count_wrap.attr('title', count);
