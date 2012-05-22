fc23bf1989f3bc1acd741cb89d49f033cfc257d5
Beanow <beanow@oscp.info>
2012-05-22 16:23:28 +0200

    Updated urls, added extra options for hardcoded values and moved to a message orientated setup vs sharing just a url.

--- jquery-socialButtons.js
+++ jquery-socialButtons.js
@@ -1,28 +1,35 @@
 /*
  * jQuery Social Buttons Plugin v0.1
- * http://
- *
+ * https://github.com/michaek/jquery-socialButtons
+ * 
  * Copyright (c) 2010 Bearded Studio LLC
  * Dual licensed under the MIT or GPL Version 2 licenses.
  * http://jquery.org/license
+ *
+ * Modified by Beanow for Tuxion - https://github.com/Tuxion/jquery-socialButtons
+ * - Improved security by using https.
+ * - Encoded urls.
+ * - Replaced facebook like popup with sharer window.
+ * - Enabled message option.
+ * - Enabled label option.
  */
 (function($, undefined){
   var services = {
     facebook: {
       display_name: 'Facebook',
-      data_url: 'http://api.facebook.com/restserver.php?method=links.getStats&format=json-strings&urls={{url}}',
-      like_url: 'http://www.facebook.com/plugins/like.php?origin={{url}}&relation=parent.parent&transport=postmessage&font=lucida%20grande&href={{url}}&layout=standard&node_type=link&sdk=joey&show_faces=false',
+      data_url: 'https://api.facebook.com/restserver.php?method=links.getStats&format=json-strings&urls={{url}}',
+      share_url: 'https://www.facebook.com/sharer/sharer.php?u={{url}}',
       count_key: 'total_count',
-      window_width: 200,
-      window_height: 80
+      window_width: 640,
+      window_height: 320
     },
-    twitter: { 
+    twitter: {
       display_name: 'Twitter',
-      data_url: 'http://urls.api.twitter.com/1/urls/count.json?url={{url}}',
-      like_url: 'http://twitter.com/share?original_referer={{url}}&url={{url}}',
+      data_url: 'https://cdn.api.twitter.com/1/urls/count.json?url={{url}}',
+      share_url: 'https://twitter.com/intent/tweet?original_referer={{url}}&url={{url}}&text={{message}}',
       count_key: 'count',
-      window_width: 500,
-      window_height: 350
+      window_width: 450,
+      window_height: 250
     }
   };
 
@@ -51,14 +58,14 @@
   }
   
   function modal(width, height, src, name) {
-    // i'd rather do this as a modal iframe, but that didn't work immediately
-    console.log("width="+width+",height="+height+",personalbar=0,toolbar=0,scrollbars=1,resizable=1");
-    window.open(src,name,"width="+width+",height="+height+",personalbar=0,toolbar=0,scrollbars=1,resizable=1")
+    window.open(src,name,"width="+width+",height="+height+",menubar=0,personalbar=0,toolbar=0,scrollbars=0,resizable=1");
   }
   
   $.fn.socialButtons = function(options){
     options = $.extend({
       url: window.location.href,
+      label: 'Share',
+      message: '',
       services: serviceKeys,
       forceRequest: false,
       friendlyCounts: true,
@@ -70,7 +77,7 @@
       
       var $buttons = $(this).addClass('social-buttons');
 
-      var $label = $('<span class="share">Share</span>')
+      var $label = $('<span class="share">'+options.label+'</span>')
         .appendTo($buttons);
 
       var $services = $('<span class="services" />')
@@ -90,7 +97,7 @@
           
           var $count_wrap = $('<a class="count" />')
             .addClass(service_name)
-            .attr('href', service.like_url.replace(/{{url}}/g, options.url))
+            .attr('href', service.share_url.replace(/{{url}}/g, encodeURIComponent(options.url)).replace(/{{message}}/g, encodeURIComponent(options.message)))
             .append('<span class="label">'+service.display_name+'</span>')
             .append('<span class="value" />')
             .appendTo($services);
@@ -101,11 +108,11 @@
           });
           
           var $count = $count_wrap.find('.value');
-
+          
           // don't make another request if we already have the stats
           if (socialStats[service_name][options.url] == undefined || options.forceRequest) {
             // get from the jsonp url
-            $.ajax(service.data_url.replace(/{{url}}/g, options.url), {
+            $.ajax(service.data_url.replace(/{{url}}/g, encodeURIComponent(options.url)), {
               dataType: 'jsonp',
               success: function(data){
                 // facebook returns an array, but we want a single object
@@ -142,6 +149,24 @@
                 }
               }
             });
+          }else{
+            var count = socialStats[service_name][options.url].count;
+            // set count for display
+            $count_wrap.attr('title', count);
+            if (options.friendlyCounts) {
+              $count.html(friendlyNumberFormat(count));
+            } else {
+              $count.html(count);
+            }
+
+            // set total for display
+            var total = totalForUrl(options.url);
+            $total_wrap.attr('title', total);
+            if (options.friendlyTotal) {
+              $total.html(friendlyNumberFormat(total));
+            } else {
+              $total.html(total);
+            }
           }
           
         }
